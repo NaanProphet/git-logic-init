cbd0e4b98e6f3878725a2709bf81c289a3a6fd35
Krishna Bhamidipati <NaanProphet@users.noreply.github.com>
2019-10-24 18:20:27 -0400

    initial script

---
+++ README.md
@@ -0,0 +1,47 @@
+# git-logic-init
+
+An init script intended for use versioning Apple Logic projects with Git.
+
+## Why
+
+Apple Logic will recalculate overviews (thumbnails) for audio files if their timestamps change—even if the checksums match. Since Git does not natively preserve timestamps, any clone will cause a rescan.
+
+![Logic project re-creating overview for audio files after Git clone](https://github.com/NaanProphet/git-logic-init/raw/master/docs/creating-overview.png)
+
+This may not seem like such a bad thing (it's rather fast) except that...**rescanning changes the checksums!!**
+
+![Checksums changed after opening Logic project - closed without saving](https://github.com/NaanProphet/git-logic-init/raw/master/docs/checksums-changed.png)
+
+This creates a nightmare for version control and increases the size of the Git repo unnecessarily!
+
+## Solution
+
+1. Use [Git LFS](https://github.com/git-lfs/git-lfs) to store large files and keep the repo size small (based on SHA 256 checksums)
+2. Use [Git Store Meta](https://github.com/danny0838/git-store-meta) to preserve timestamps
+
+Since Git commit hooks are scripts, they must—by design—be re-configured each time a repository is created/cloned.
+
+## Usage
+
+### Initial
+
+* Create a new folder
+  * Open Terminal
+  * Create a new folder `mkdir myrepo`
+  * `cd` into the new folder `myrepo`
+* Pull the latest script from GitHub Releases `curl -L -O https://github.com/NaanProphet/git-logic-init/releases/latest/download/init.sh`
+* Run script `sh init.sh`
+  * This will bootstrap the commit hooks and create the `.githooks` folder (which can be committed into source control)
+  * This will also run the command `git config core.hooksPath .githooks` at the end to setup the custom hooks folder
+* Commit!
+
+### Cloning
+
+To re-initialize a repo that already has a `.githooks` folder, simply run `sh init.sh` again.
+
+## Dependencies
+
+* Homebrew
+* Git (>= 2.9)
+* Git LFS
+* Git Store Meta
---
+++ src/init.sh
@@ -0,0 +1,168 @@
+#!/bin/bash
+
+### This script is used to bootstrap a blank Apple Logic repo with the
+### required Git commit hooks for Git LFS and Git Store Meta.
+### It stores the hooks in a custom Hooks location called .githooks
+### which can then be committed into source control.
+###
+### If the .githooks folder is already present, then this script
+### bypasses bootstrapping and simply re-initializes the .githooks
+### folder. This is required after each new clone.
+###
+### See https://github.com/danny0838/git-store-meta for more info about
+### how file timestamps are preserved with commit hooks.
+###
+### Platform: Mac OS X
+### Dependencies: Homebrew, Git (>= 2.9), Git LFS, Git Store Meta
+###
+### Development notes:
+### - The bootstrapping process is NOT idempotent.
+### - To start over, remove all temp files using: `rm -rf .git*`
+###
+### Author: Krishna Bhamidipati (NaanProphet)
+### version: 0.1
+###
+
+set -e
+# Any subsequent(*) commands which fail will cause the shell script to exit immediately
+# Special thanks to: https://stackoverflow.com/a/2871034/1603489
+
+
+LFS_TYPES=('*.wav' '*.aif' '*.aiff' '*.mp3' '*.m4a' '*.zip' '*.gz' '*.tgz')
+GIT_META_VERSION=2.0.1
+HOOKS_DIR="./.git/hooks"
+DEST_DIR=".githooks"
+MIN_GIT_VERSION="2.9"
+
+# -----------------------------------------------------------------------------
+
+function version_lt() {
+  # Special thanks to: http://ask.xmodulo.com/compare-two-version-numbers.html
+  test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" != "$1";
+}
+
+backup_hook () {
+  local hook_name=$1
+  local hook_path="${HOOKS_DIR}/${hook_name}"
+
+  if [ -f "${hook_path}" ]; then
+    echo "Backing up exiting ${hook_name} hook"
+    mv "${hook_path}" "${hook_path}.orig"
+  fi
+}
+
+merge_hook () {
+  local hook_name=$1
+  local hook_path="${HOOKS_DIR}/${hook_name}"
+
+  if [ -f "${hook_path}.orig" ]; then
+    mv "${hook_path}" "${hook_path}.meta"
+    cp "${hook_path}.orig" "${hook_path}"
+    # exclude double shebang
+    echo "" >> "${hook_path}"
+    cat "${hook_path}.meta" | sed '/^#!/ d' >> "${hook_path}"
+    echo "Merged changes into ${hook_name} hook"
+  fi
+}
+
+bootstrap_repo () {
+
+  # Initialize Git
+
+  git init
+
+  # Initialize Git LFS
+
+  git lfs install
+  for t in ${LFS_TYPES[@]}; do
+    git lfs track $t
+  done
+
+  # Preserve file timestamps of Audio Files. Otherwise Logic
+  # will re-calculate waveforms after checkout and change
+  # the checksums!! Special thanks to:
+  # https://github.com/danny0838/git-store-meta
+  #
+  # Run install to write commit hooks pre-commit, post-checkout
+  # and post-merge *before* LFS' hooks. Otherwise the `install`
+  # command will fail.
+
+  # move originals from LFS, if present
+  backup_hook pre-commit
+  backup_hook post-checkout
+  backup_hook post-merge
+
+  echo "Initializing Git Store Meta"
+  # creates pre-commit, post-checkout and post-merge
+  git-store-meta.pl --install
+
+  # merge with originals and remove double shebang if present
+  merge_hook pre-commit
+  merge_hook post-checkout
+  merge_hook post-merge
+
+  # cleanup any files left behind
+  rm -f ${HOOKS_DIR}/pre-commit.meta ${HOOKS_DIR}/pre-commit.orig \
+    ${HOOKS_DIR}/post-checkout.meta ${HOOKS_DIR}/post-checkout.orig \
+    ${HOOKS_DIR}/post-merge.meta ${HOOKS_DIR}/post-merge.orig \
+    ${HOOKS_DIR}/*.sample
+
+  # move hooks folder out of .git so it can be committed
+  mv -f "${HOOKS_DIR}" "${DEST_DIR}"
+
+  # copy git-store-meta locally to make clones easier for others
+  cp `which git-store-meta.pl` "${DEST_DIR}/"
+
+}
+
+# -----------------------------------------------------------------------------
+
+# Check for binaries
+
+if ! [ -x "$(command -v brew)" ]; then
+  echo "\033[31m Homebrew is not installed. \033[0m" >&2
+  echo "\033[31m Visit https://brew.sh for one-line install instructions. \033[0m"
+  exit 1
+fi
+
+if ! [ -x "$(command -v git)" ]; then
+  echo "\033[31m Git is not installed. Use \`brew install git\` to install. \033[0m" >&2
+  exit 1
+fi
+
+# check minimum version for git config core.hooksPath
+git_version=`git --version | perl -pe '($_)=/([0-9]+([.][0-9]+)+)/'`
+if version_lt $git_version $MIN_GIT_VERSION; then
+  echo "\033[31m Git version $git_version must be >= $MIN_GIT_VERSION. Use \`brew upgrade git\` to upgrade. \033[0m" >&2
+  exit 1
+fi
+
+if ! [ -x "$(command -v git-lfs)" ]; then
+  echo "\033[31m Git LFS is not installed. Use \`brew install git-lfs\` to install. \033[0m" >&2
+  exit 1
+fi
+
+if ! [ -x "$(command -v git-store-meta.pl)" ] && ! [ -f "${DEST_DIR}/git-store-meta.pl" ]; then
+  echo "\033[31m Git Store Meta is not installed. \033[0m" >&2
+  echo "\033[31m Use \`brew cask install NaanProphet/ninjabrew/git-store-meta\` to install. \033[0m" >&2
+  exit 1
+fi
+
+# Check ignore rules. Assume owner of repo has access to all plugins so
+# that Freeze Files are not needed. Otherwise, manually zip up the Freeze Files
+# folder if needed to the archive
+#
+# Special thanks to:
+# https://stackoverflow.com/a/3557165/1603489
+touch .gitignore
+grep -qxF '.DS_Store' .gitignore || echo '.DS_Store' >> .gitignore
+grep -qxF 'Freeze Files' .gitignore || echo 'Freeze Files' >> .gitignore
+grep -qxF '*.rxdoc' .gitignore || echo '*.rxdoc' >> .gitignore
+
+if ! [ -d "${DEST_DIR}" ]; then
+  bootstrap_repo
+fi
+
+# initialize hooks location (must be done after each clone)
+git config core.hooksPath "${DEST_DIR}"
+echo "${DEST_DIR} successfully initialized"
